306ccc8976e14d146cc4764ff87f2f190ed191c2
Luis Amaro <50186355+feramaro@users.noreply.github.com>
2019-06-26 15:24:29 -0300

    Código documentado

--- cripto.js
+++ cripto.js
@@ -1,17 +1,23 @@
-var inPalavra = document.getElementById("inPalavra")
+// Importa os Campos do HTML
+
+var inPalavra = document.getElementById("inPalavra") 
 var outTexto = document.getElementById("outTexto")
 
+// Cria os vetores para salvaar a palavra normal e encriptada
 var pSalva = []
 var pCript = []
 
 function criptografar(){
     var palavra = inPalavra.value
-    if(pSalva.length == 1){
+
+    if(pSalva.length == 1){ // Remove a palavra atual no vetor (caso possuir)
         pSalva.shift()
     }
-    pSalva.push(palavra)
+    pSalva.push(palavra) // Adiciona a palavra no vetor
 
-    var r = ""
+    var r = "" // Concatena a palavra criptografada
+
+    // Laços para obter letras ímpares e pares
 
     for(var i = 1; i < palavra.length; i+=2){
         r+= palavra.charAt(i).toUpperCase()
@@ -20,23 +26,23 @@ function criptografar(){
     for(var j = 0; j < palavra.length; j+=2){
         r+= palavra.charAt(j).toLowerCase()
     }
-    if (pCript.length == 1){
+    if (pCript.length == 1){ // Remove a palavra criptografada do vetor (caso possuir)
         pCript.shift()
     }
-    pCript.push(r)
-    outTexto.textContent = `Resultado: ${r}`
+    pCript.push(r) // Adiciona a palavra critografada no vetor
+    outTexto.textContent = `Resultado: ${r}` // Exibe o resultado
 
 }
 
 function decriptografar(){
     var palavra = inPalavra.value
 
-    if(palavra != pCript[0]){
+    if(palavra != pCript[0]){ // Verifica se a palavra inserida é a palavra criptografada
         alert("Insira a Palavra Correta")
         return
     } else {
-    if(pCript.length == 1 && pSalva.length == 1){
-        outTexto.textContent = `Resultado: ${pSalva[0]}`
+        if(pCript.length == 1 && pSalva.length == 1){ // Caso seja, a palavra decriptografada é exibida
+            outTexto.textContent = `Resultado: ${pSalva[0]}`
     }
 }
     
@@ -44,6 +50,6 @@ function decriptografar(){
 
 
 
-
+// Importa e adiciona o evento click aos botões
 var btDecript = document.getElementById("btDecript").addEventListener("click", decriptografar)
 var btCript = document.getElementById("btCript").addEventListener("click", criptografar)
